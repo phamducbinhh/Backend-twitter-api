70f9aab06d9accbcf84079d382aed150a7f05a84
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-21 14:42:38 +0700

    update filterMiddleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import { TokenType } from '~/constants/enums'
 import userController from '~/controllers/user.controllers'
 import asyncHandler from '~/middlewares/asyncHandler'
+import { filterMiddleware } from '~/middlewares/common.middlewares'
 import { verifiedUserValidator, verifyToken } from '~/middlewares/jwtMiddleware'
 import { validate } from '~/middlewares/validate'
 import { ForgotPasswordSchema } from '~/schema/user/forgot-password'
 import { ResetPasswordSchema } from '~/schema/user/reset-password'
 import { UpdateProfileSchema } from '~/schema/user/update-profile'
 import { VerifyEmailSchema } from '~/schema/user/verify-email'
+import { UpdateMeReqBody } from '~/types/users.type'
 const router = express.Router()
 
 router.post(
@@ -35,6 +37,16 @@ router.patch(
   verifyToken(TokenType.AccessToken),
   verifiedUserValidator,
   validate(UpdateProfileSchema),
+  filterMiddleware<UpdateMeReqBody>([
+    'name',
+    'date_of_birth',
+    'bio',
+    'location',
+    'website',
+    'username',
+    'avatar',
+    'cover_photo'
+  ]),
   asyncHandler(userController.updateProfile)
 )
 
