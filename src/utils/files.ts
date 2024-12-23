810e5e993d80873b70d742275627bc89f946ec5c
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-23 14:13:29 +0700

    update

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -15,12 +15,23 @@ export const handleUploadSingleImages = async (req: Request) => {
     uploadDir: path.resolve('uploads'),
     maxFields: 1,
     keepExtensions: true,
-    maxFileSize: 300 * 1024 // 300kb
+    maxFileSize: 300 * 1024, // 300kb
+    filter: function ({ name, mimetype }) {
+      const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('File type is not valid') as any)
+      }
+      return valid
+    }
   })
 
-  return await new Promise<{ files: formidable.Files }>((resolve, reject) => {
+  return await new Promise<File[]>((resolve, reject) => {
     form.parse(req as any, (err, fields, files) => {
       if (err) return reject(err)
+
+      if (!files.image) {
+        return reject(new Error('File is empty'))
+      }
       resolve(files as any)
     })
   })
