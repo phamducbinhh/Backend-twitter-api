01cae5ed6fc617db925a68ae2ccdfb690720d66c
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-24 16:27:21 +0700

    update

--- src/services/media.service.ts
+++ src/services/media.service.ts
@@ -2,6 +2,7 @@ import { File } from 'formidable'
 import fs from 'fs'
 import path from 'path'
 import sharp from 'sharp'
+import { envConfig, isProduction } from '~/constants/config'
 import { UPLOAD_IMAGE_DIR } from '~/constants/dir'
 import { HttpStatusCode } from '~/constants/HttpStatusCode'
 import { USERS_MESSAGES } from '~/constants/messages'
@@ -25,11 +26,11 @@ class MediaService {
     fs.unlinkSync(inputPath)
 
     // Tạo URL đầy đủ cho ảnh
-    const imageUrl = `${req.protocol}://${req.get('host')}/uploads/images/${outputFileName}`
+    const imageUrl = `${req.protocol}://${req.get('host')}/static/image/${outputFileName}`
 
     // Trả về phản hồi
     return handleResponse(HttpStatusCode.SUCCESS, true, USERS_MESSAGES.UPLOAD_SUCCESS, {
-      url: imageUrl
+      url: isProduction ? `${envConfig.port}/static/images/${outputFileName}` : imageUrl
     })
   }
 }
