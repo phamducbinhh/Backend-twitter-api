6a63c8459a633e6bd6efed5f15d1dbc5ac42a68a
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-24 15:51:42 +0700

    update done format image

--- src/services/media.service.ts
+++ src/services/media.service.ts
@@ -1,13 +1,30 @@
+import { File } from 'formidable'
+import fs from 'fs'
+import path from 'path'
+import sharp from 'sharp'
+import { UPLOAD_IMAGE_DIR } from '~/constants/dir'
 import { HttpStatusCode } from '~/constants/HttpStatusCode'
 import { USERS_MESSAGES } from '~/constants/messages'
-import { handleUploadSingleImages } from '~/utils/files'
+import { handleUploadImage } from '~/utils/files'
 import { handleResponse } from '~/utils/response'
 
 class MediaService {
   async uploadImageMedia(req: any) {
-    const data = await handleUploadSingleImages(req)
+    const files: File[] = await handleUploadImage(req)
+    const uploadedFile = files[0]
+    const inputPath = uploadedFile.filepath
 
-    return handleResponse(HttpStatusCode.SUCCESS, true, USERS_MESSAGES.UPLOAD_SUCCESS, data)
+    const outputDir = path.resolve(UPLOAD_IMAGE_DIR)
+    const outputFileName = `processed-${Date.now()}.webp`
+    const outputPath = path.join(outputDir, outputFileName)
+
+    // Thực hiện chuyển đổi sang WebP
+    const resolve = await sharp(inputPath).webp({ quality: 80 }).toFile(outputPath)
+
+    // Xóa file gốc nếu cần
+    fs.unlinkSync(inputPath)
+
+    return handleResponse(HttpStatusCode.SUCCESS, true, USERS_MESSAGES.UPLOAD_SUCCESS, resolve)
   }
 }
 
--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,18 +1,20 @@
-import formidable from 'formidable'
+import formidable, { File } from 'formidable'
 import fs from 'fs'
 import path from 'path'
+import { UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_TEMP_DIR } from '~/constants/dir'
 export const initFolder = () => {
-  const uploadsDir = path.resolve('uploads')
-  if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir, {
-      recursive: true
-    })
-  }
+  ;[UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_TEMP_DIR].forEach((dir) => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {
+        recursive: true // mục đích là để tạo folder nested
+      })
+    }
+  })
 }
 
-export const handleUploadSingleImages = async (req: Request) => {
+export const handleUploadImage = async (req: Request) => {
   const form = formidable({
-    uploadDir: path.resolve('uploads'),
+    uploadDir: path.resolve(UPLOAD_IMAGE_TEMP_DIR),
     maxFields: 1,
     keepExtensions: true,
     maxFileSize: 300 * 1024, // 300kb
@@ -29,10 +31,18 @@ export const handleUploadSingleImages = async (req: Request) => {
     form.parse(req as any, (err, fields, files) => {
       if (err) return reject(err)
 
-      if (!files.image) {
+      const file = files.image // Lấy file từ trường `image`
+      if (!file) {
         return reject(new Error('File is empty'))
       }
-      resolve(files as any)
+
+      resolve(file as File[]) // Trả về file đã upload
     })
   })
+}
+
+export const getNameFromFullname = (fullname: string) => {
+  const namearr = fullname.split('.')
+  namearr.pop()
+  return namearr.join('')
 }
