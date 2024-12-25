16fea00c2fd720ed6a1a86ebec560be1c78725ff
Phạm Đức Bình <binhpd@oeg.vn>
2024-12-25 09:27:19 +0700

    update

--- src/routes/media.routes.ts
+++ src/routes/media.routes.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import { TokenType } from '~/constants/enums'
 import mediaController from '~/controllers/media.controllers'
 import asyncHandler from '~/middlewares/asyncHandler'
+import { verifiedUserValidator, verifyToken } from '~/middlewares/jwtMiddleware'
 
 const router = express.Router()
 
-router.post('/upload-media', asyncHandler(mediaController.uploadImageMedia))
+router.post(
+  '/upload-image',
+  verifyToken(TokenType.AccessToken),
+  verifiedUserValidator,
+  asyncHandler(mediaController.uploadImageMedia)
+)
 
 export default router
