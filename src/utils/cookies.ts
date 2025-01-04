d430f799d0892ecbfb0b42f27ceace8fc53de61f
Phạm Đức Bình <binhpd@oeg.vn>
2025-01-04 14:18:31 +0700

    update

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -2,6 +2,7 @@ export const setTokenCookie = (res: any, token: string) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Strict',
     maxAge: 24 * 60 * 60 * 1000
   })
 }
