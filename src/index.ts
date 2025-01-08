41da519af6c5ee087344a7e2a2a6846a9417de3a
Phạm Đức Bình <binhpd@oeg.vn>
2025-01-08 16:39:13 +0700

    Update Rate Limit

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,8 @@ const corsOptions = {
 }
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 60 * 1000,
+  max: 200,
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false // Disable the `X-RateLimit-*` headers
   // store: ... , // Use an external store for more precise rate limiting
